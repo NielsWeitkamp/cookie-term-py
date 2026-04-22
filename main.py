8dca85bd1b015b99010f5d650fc9828e20e27320
NielsWeitkamp <weitkamp.niels@gmail.com>
2026-04-22 09:36:25 +0200

    Created the base command input Created cookieAmount and cookieClickValue variables

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 cookieAmount = 0
+cookieClickValue = 1
+
+
+def mainLoop():
+    commandInput = input(">>> ")
+    mainCommandParts = commandInput.split()
+    mainCommand = mainCommandParts[0]
+    commandArguments = mainCommandParts[1:]
+
+    match mainCommand:
+        case "cookie":
+            if not commandArguments:
+                print("No argument")
+            elif commandArguments[0] in ["-h", "--help"]:
+                print("help")
+            else:
+                print("no help")
+
+    print("\n", mainCommand)
+    print(commandArguments)
+
+
+mainLoop()
