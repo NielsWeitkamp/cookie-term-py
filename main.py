43f33e4a9a39a3be21a63240d9556f3bd172cb0e
NielsWeitkamp <weitkamp.niels@gmail.com>
2026-04-26 21:24:45 +0200

    Added the building values as one variable called "buildings"
    A message is now displayed when no command is input
    
    A mssage is now displayed  when a command is input that does not exist.

--- main.py
+++ main.py
@@ -2,6 +2,64 @@ from colorama import *
 
 cookieAmount = 0
 cookieClickValue = 1
+buildings = {
+    "cursor": {"price": 15, "amount": 0, "generationAmount": 0.1},
+    "grandma": {"price": 100, "amount": 0, "generationAmount": 1},
+    "farm": {"price": 1_100, "amount": 0, "generationAmount": 8},
+    "mine": {"price": 12_000, "amount": 0, "generationAmount": 47},
+    "factory": {"price": 130_000, "amount": 0, "generationAmount": 260},
+    "bank": {"price": 1_400_000, "amount": 0, "generationAmount": 1_400},
+    "temple": {"price": 20_000_000, "amount": 0, "generationAmount": 7_800},
+    "wizardtower": {"price": 330_000_000, "amount": 0, "generationAmount": 44_000},
+    "shipment": {"price": 5_100_000_000, "amount": 0, "generationAmount": 260_000},
+    "alchemylab": {"price": 75_000_000_000, "amount": 0, "generationAmount": 1_600_000},
+    "portal": {"price": 1_000_000_000_000, "amount": 0, "generationAmount": 10_000_000},
+    "timemachine": {
+        "price": 14_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 65_000_000,
+    },
+    "antimattercondenser": {
+        "price": 170_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 430_000_000,
+    },
+    "prism": {
+        "price": 2_100_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 2_900_000_000,
+    },
+    "chancemaker": {
+        "price": 26_000_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 21_000_000_000,
+    },
+    "fractalengine": {
+        "price": 310_000_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 150_000_000_000,
+    },
+    "editedfork": {
+        "price": 71_000_000_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 1_100_000_000_000,
+    },
+    "idleverse": {
+        "price": 12_000_000_000_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 8_300_000_000_000,
+    },
+    "cortexbaker": {
+        "price": 1_900_000_000_000_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 64_000_000_000_000,
+    },
+    "you": {
+        "price": 540_000_000_000_000_000_000_000_000,
+        "amount": 0,
+        "generationAmount": 512_000_000_000_000,
+    },
+}
 
 
 def mainLoop():
@@ -11,7 +69,7 @@ def mainLoop():
     mainCommandParts = commandInput.split()
 
     if not mainCommandParts:
-        mainCommand = [" "]
+        mainCommand = " "
     else:
         mainCommand = mainCommandParts[0]
 
@@ -55,6 +113,12 @@ def mainLoop():
                     Fore.RED + "error:",
                     Fore.WHITE + "no operation specified (use -h for help)",
                 )
+
+        case " ":
+            print("no comment")
+
+        case _:
+            print(mainCommand + ":", "no command found")
     mainLoop()
 
     print("\n", mainCommand)
